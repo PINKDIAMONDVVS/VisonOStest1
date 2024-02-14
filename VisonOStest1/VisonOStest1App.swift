96c1f06ee12075f69649a579973131b107697f72
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-13 22:41:39 -0500

    Initial Commit

---
+++ VisonOStest1/VisonOStest1App.swift
@@ -0,0 +1,17 @@
+//
+//  VisonOStest1App.swift
+//  VisonOStest1
+//
+//  Created by Junyi Chen on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VisonOStest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
