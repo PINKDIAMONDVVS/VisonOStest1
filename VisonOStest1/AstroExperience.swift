e688e6ba9c55618b5ca1cf9655c9408cd3edc568
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-14 02:06:17 -0500

    astro 1

---
+++ VisonOStest1/AstroExperience.swift
@@ -0,0 +1,18 @@
+//
+//  AstroExperience.swift
+//  VisonOStest1
+//
+//  Created by Junyi Chen on 2/14/24.
+//
+
+import SwiftUI
+
+struct AstroExperience: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstroExperience()
+}
